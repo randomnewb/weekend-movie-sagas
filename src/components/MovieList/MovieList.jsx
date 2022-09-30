c4e3a8a5e8c9fefb93426cba2f766b14fcbb63db
Tu Nguyen <nguy1581@gmail.com>
2022-09-29 22:36:22 -0500

    Added onClick to images on MovieList to go to /detail Route page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,31 +1,36 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+// Import useHistory so we can go to '/detail'
+import { useHistory } from "react-router-dom";
+import "./MovieList.css";
 
 function MovieList() {
-
     const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+    const movies = useSelector((store) => store.movies);
+    const history = useHistory();
 
     useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
+        dispatch({ type: "FETCH_MOVIES" });
     }, []);
 
     return (
         <main>
             <h1>MovieList</h1>
             <section className="movies">
-                {movies.map(movie => {
+                {movies.map((movie) => {
                     return (
-                        <div key={movie.id} >
+                        <div key={movie.id}>
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img
+                                src={movie.poster}
+                                alt={movie.title}
+                                onClick={() => history.push("/detail")}
+                            />
                         </div>
                     );
                 })}
             </section>
         </main>
-
     );
 }
 
