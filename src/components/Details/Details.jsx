75086c23b0374b0699b08ec009bd1b667858b154
Tu Nguyen <nguy1581@gmail.com>
2022-09-29 22:49:06 -0500

    Setup Details component a bit more to show movie information, but it shows all movies

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,7 +1,31 @@
 import { React } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Details = () => {
-    return null;
+    // We'll use sagas to dispatch our "FETCH_MOVIES"
+    const dispatch = useDispatch();
+    const movies = useSelector((store) => store.movies);
+
+    // dispatch/sagas axios GET request goes here
+
+    // could add useEffect here to show items on page load
+
+    // need to rewrite this to only show clicked on movie (probably via its ID in database)
+    return (
+        <div>
+            <ul>
+                {movies.map((movie) => (
+                    <li key={movie.id}>
+                        {movie.id}
+                        {movie.title}
+                        {movie.poster}
+                        {movie.description}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Details;
